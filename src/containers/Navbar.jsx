f6956964d74d9d066765665c534047b4ebe1eb17
Poorvi <65484904+PoorviShetty@users.noreply.github.com>
2022-01-18 12:51:25 +0530

    Update Navbar.jsx

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/gdsc-logo.PNG";
+import logo from "../assets/gdsc-logo.png";
 
 function Navbar() {
   return (
